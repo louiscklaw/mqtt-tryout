721a587a8b96be23ceced26b4efdea3d21fcb82a
logickee <logickee@gmail.com>
2019-06-08 15:43:31 +0800

    update,

--- tryout/webserver.ino
+++ tryout/webserver.ino
@@ -9,7 +9,7 @@ float humidity = 0.0;
 String example_html = "";
 
 String SendHTML(float Temperaturestat, float Humiditystat) {
-  example_html = String(Temperaturestat);
+  example_html = String(Temperaturestat) + "," + String(Humiditystat);
   return example_html;
 }
 
